769334d32f6d5d541d97cc3a3d44d52d4f8adb20
Diadochokinetic <Diadochokinetic@gmail.com>
2024-08-18 21:23:27 +0200

    Fix type check.

--- aktipp/etl/_helper.py
+++ aktipp/etl/_helper.py
@@ -4,7 +4,7 @@ import polars as pl
 def _suffix_alias(cols: str | list[str], suffix: str) -> pl.Expr | list[pl.Expr]:
     if isinstance(cols, str):
         return pl.col(cols).alias(f"{cols}{suffix}")
-    elif isinstance(cols, list[str]):
+    elif isinstance(cols, list):
         expressions = []
         for col in cols:
             expressions.append(pl.col(col).alias(f"{col}{suffix}"))
